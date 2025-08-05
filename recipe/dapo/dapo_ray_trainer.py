80c8a725f218d24247ecdcae35ad609240d492b2
lishuaibin <lishuaibin@pjlab.org.cn>
2025-08-05 05:23:03 +0000

    cl

--- recipe/dapo/dapo_ray_trainer.py
+++ recipe/dapo/dapo_ray_trainer.py
@@ -273,6 +273,7 @@ class RayDAPOTrainer(RayPPOTrainer):
 
                     # compute global_valid tokens
                     batch.meta_info["global_token_num"] = torch.sum(batch.batch["attention_mask"], dim=-1).tolist()
+                    batch.meta_info["global_step"] = torch.tensor([self.global_steps]).tolist()
 
                     # recompute old_log_probs
                     with marked_timer("old_log_prob", timing_raw, "blue"):
